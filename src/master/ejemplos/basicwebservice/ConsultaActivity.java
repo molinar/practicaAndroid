676f1970a8e2e63a9966032e5fc12f4f7cd7fb56
Javier Molina <jmolinaromero86@gmail.com>
2014-11-12 13:58:16 +0100

    Segundo día practica

--- src/master/ejemplos/basicwebservice/ConsultaActivity.java
+++ src/master/ejemplos/basicwebservice/ConsultaActivity.java
@@ -1,13 +1,86 @@
 package master.ejemplos.basicwebservice;
 
+import java.util.ArrayList;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.EditText;
 
 public class ConsultaActivity extends Activity {
+	ArrayList<Personas> listaPersonas = new ArrayList<Personas>();
+	int numero_registro = 0;
+	
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_consulta);
+		
+		Intent intent = getIntent();
+        String resultado = intent.getStringExtra("respuesta");
+        stringToArray(resultado);
+        mostrar();
+	}
+	
+	public void stringToArray(String resultado){
+		try{
+			JSONArray json = new JSONArray(resultado);
+			for(int i = 1; i < json.length();i++){
+				Personas personas = new Personas();
+				JSONObject jsonArrayChild = json.getJSONObject(i);
+				personas.setDni(jsonArrayChild.getString("DNI"));
+				personas.setNombre(jsonArrayChild.getString("Nombre"));
+				personas.setApellidos(jsonArrayChild.getString("Apellidos"));
+				personas.setDireccion(jsonArrayChild.getString("Direccion"));
+				personas.setTelefono(jsonArrayChild.getString("Telefono"));
+				personas.setEquipo(jsonArrayChild.getString("Equipo"));
+				listaPersonas.add(personas);
+				//Log.d("MainActivity", personas.getDni());
+				//Log.d("MainActivity", personas.getNombre());
+				//Log.d("MainActivity", personas.getApellidos());
+			}
+		}catch (Exception e){
+			Log.e("BasicWebService",e.toString());
+			e.printStackTrace();
+		}
+	}
+	
+	public void mostrar(){
+		EditText dni = (EditText)findViewById(R.id.editText1);
+		EditText nombre = (EditText)findViewById(R.id.EditText01);
+		EditText apellidos = (EditText)findViewById(R.id.editText2);
+		EditText direccion = (EditText)findViewById(R.id.editText3);
+		EditText telefono = (EditText)findViewById(R.id.editText4);
+		EditText equipo = (EditText)findViewById(R.id.EditText02);
+		nombre.setText(listaPersonas.get(numero_registro).getNombre());
+		dni.setText(listaPersonas.get(numero_registro).getDni());
+		apellidos.setText(listaPersonas.get(numero_registro).getApellidos());
+		direccion.setText(listaPersonas.get(numero_registro).getDireccion());
+		telefono.setText(listaPersonas.get(numero_registro).getTelefono());
+		equipo.setText(listaPersonas.get(numero_registro).getEquipo());
+	}
+	
+	public void mostrarPrimero(){
+		numero_registro = 0;
+		mostrar();
+	}
+	
+	public void mostrarUltimo(){
+		numero_registro = 7;
+		mostrar();
+	}
+	
+	public void mostrarSiguiente(){
+		numero_registro++;
+		mostrar();
+	}
+	
+	public void mostrarAnterior(){
+		numero_registro--;
+		mostrar();
 	}
 }
--- src/master/ejemplos/basicwebservice/MainActivity.java
+++ src/master/ejemplos/basicwebservice/MainActivity.java
@@ -1,14 +1,11 @@
 package master.ejemplos.basicwebservice;
 
-import java.util.ArrayList;
-
 import org.apache.http.HttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.util.EntityUtils;
-import org.json.JSONArray;
-import org.json.JSONObject;
 import android.app.Activity;
 import android.app.ProgressDialog;
+import android.content.Intent;
 import android.net.http.AndroidHttpClient;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -20,7 +17,6 @@ import android.widget.Toast;
 public class MainActivity extends Activity {
 	
 	private EditText dni;
-	ArrayList<Personas> listaPersonas = new ArrayList<Personas>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,12 +25,16 @@ public class MainActivity extends Activity {
     }
     
     public void conectar(View v){
-    	new ConsultaBD().execute(dni.getText().toString());
-
+    	if (dni.getText().length() > 8){
+        	Toast.makeText(this, "El DNI introducido no es valido", Toast.LENGTH_LONG).show();
+    	}else{
+    		new ConsultaBD().execute(dni.getText().toString());
+    	}
     }
     
     private class ConsultaBD extends AsyncTask<String, String, String>{
-       	private ProgressDialog pDialog;
+       	private final int CONSULTA = 0;
+		private ProgressDialog pDialog;
     	private final String url = "http://demo.calamar.eui.upm.es/dasmapi/v1/miw26/fichas";
 
 		@Override
@@ -53,34 +53,26 @@ public class MainActivity extends Activity {
 			if(!dnis[0].equals("")){
 				url_final +="/"+dnis[0];
 			}
+			AndroidHttpClient httpclient = null;
 			try{
-				AndroidHttpClient httpclient = AndroidHttpClient.newInstance("AndroidHttpClient");
+				httpclient = AndroidHttpClient.newInstance("AndroidHttpClient");
 				HttpGet httpget = new HttpGet(url_final);
 				HttpResponse response = httpclient.execute(httpget);
 				respuesta = EntityUtils.toString(response.getEntity());
-				
-				JSONObject json = new JSONObject(respuesta);
-				JSONArray jsonArray = json.optJSONArray("personas");
-				for(int i = 0; i < jsonArray.length();i++){
-					Personas personas = new Personas();
-					JSONObject jsonArrayChild = jsonArray.getJSONObject(i);
-					personas.setDni(jsonArrayChild.optString("dni"));
-					personas.setNombre(jsonArrayChild.optString("nombre"));
-					personas.setApellidos(jsonArrayChild.optString("apellidos"));
-					personas.setDireccion(jsonArrayChild.optString("direccion"));
-					personas.setTelefono(jsonArrayChild.optString("telefono"));
-					personas.setEquipo(jsonArrayChild.optString("equipo"));
-					listaPersonas.add(personas);
-				}
+				httpclient.close();
 			}catch (Exception e){
 				Log.e("BasicWebService",e.toString());
+				e.printStackTrace();
+			
 			}
 			return respuesta;
 		}
 		
     	protected void onPostExecute(String respuesta) {
     		pDialog.dismiss();
-    		Toast.makeText(getBaseContext(), respuesta, Toast.LENGTH_LONG).show();
+    		Intent i = new Intent(MainActivity.this, ConsultaActivity.class);	
+    		i.putExtra("respuesta", respuesta.toString());
+    		startActivityForResult(i,CONSULTA);
 		}
     }
 
